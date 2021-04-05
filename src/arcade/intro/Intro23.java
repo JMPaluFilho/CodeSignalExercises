3e198c5c5b51a97db2304c23272b3e0af999db57
JMPaluFilho <jmpalufilho@gmail.com>
2021-04-04 21:38:58 -0300

    Finished Intro23

--- src/arcade/intro/Intro23.java
+++ src/arcade/intro/Intro23.java
@@ -1,93 +1,106 @@
 package arcade.intro;
 
+import java.util.Arrays;
+
 public class Intro23 {
-	
-	/*
-	 * Last night you partied a little too hard. Now there's a black and white photo
-	 * of you that's about to go viral! You can't let this ruin your reputation, so
-	 * you want to apply the box blur algorithm to the photo to hide its content.
-	 * 
-	 * The pixels in the input image are represented as integers. The algorithm
-	 * distorts the input image in the following way: Every pixel x in the output
-	 * image has a value equal to the average value of the pixel values from the 3 ×
-	 * 3 square that has its center at x, including x itself. All the pixels on the
-	 * border of x are then removed.
-	 * 
-	 * Return the blurred image as an integer, with the fractions rounded down.
-	 * 
-	 * Example
-	 * 
-	 * For
-	 * 
-	 * image = [[1, 1, 1], [1, 7, 1], [1, 1, 1]] the output should be boxBlur(image)
-	 * = [[1]].
-	 * 
-	 * To get the value of the middle pixel in the input 3 × 3 square: (1 + 1 + 1 +
-	 * 1 + 7 + 1 + 1 + 1 + 1) = 15 / 9 = 1.66666 = 1. The border pixels are cropped
-	 * from the final result.
-	 * 
-	 * For
-	 * 
-	 * image = [[7, 4, 0, 1], [5, 6, 2, 2], [6, 10, 7, 8], [1, 4, 2, 0]] the output
-	 * should be
-	 * 
-	 * boxBlur(image) = [[5, 4], [4, 4]] There are four 3 × 3 squares in the input
-	 * image, so there should be four integers in the blurred output. To get the
-	 * first value: (7 + 4 + 0 + 5 + 6 + 2 + 6 + 10 + 7) = 47 / 9 = 5.2222 = 5. The
-	 * other three integers are obtained the same way, then the surrounding integers
-	 * are cropped from the final result.
-	 * 
-	 * Input/Output
-	 * 
-	 * [execution time limit] 3 seconds (java)
-	 * 
-	 * [input] array.array.integer image
-	 * 
-	 * An image, stored as a rectangular matrix of non-negative integers.
-	 * 
-	 * Guaranteed constraints: 3 ≤ image.length ≤ 100, 3 ≤ image[0].length ≤ 100, 0
-	 * ≤ image[i][j] ≤ 255.
-	 * 
-	 * [output] array.array.integer
-	 * 
-	 * A blurred image represented as integers, obtained through the process in the
-	 * description.
-	 */
+
+    /*
+     * Last night you partied a little too hard. Now there's a black and white photo
+     * of you that's about to go viral! You can't let this ruin your reputation, so
+     * you want to apply the box blur algorithm to the photo to hide its content.
+     *
+     * The pixels in the input image are represented as integers. The algorithm
+     * distorts the input image in the following way: Every pixel x in the output
+     * image has a value equal to the average value of the pixel values from the 3 ×
+     * 3 square that has its center at x, including x itself. All the pixels on the
+     * border of x are then removed.
+     *
+     * Return the blurred image as an integer, with the fractions rounded down.
+     *
+     * Example
+     *
+     * For
+     *
+     * image = [[1, 1, 1], [1, 7, 1], [1, 1, 1]] the output should be boxBlur(image)
+     * = [[1]].
+     *
+     * To get the value of the middle pixel in the input 3 × 3 square: (1 + 1 + 1 +
+     * 1 + 7 + 1 + 1 + 1 + 1) = 15 / 9 = 1.66666 = 1. The border pixels are cropped
+     * from the final result.
+     *
+     * For
+     *
+     * image = [[7, 4, 0, 1], [5, 6, 2, 2], [6, 10, 7, 8], [1, 4, 2, 0]] the output
+     * should be
+     *
+     * boxBlur(image) = [[5, 4], [4, 4]] There are four 3 × 3 squares in the input
+     * image, so there should be four integers in the blurred output. To get the
+     * first value: (7 + 4 + 0 + 5 + 6 + 2 + 6 + 10 + 7) = 47 / 9 = 5.2222 = 5. The
+     * other three integers are obtained the same way, then the surrounding integers
+     * are cropped from the final result.
+     *
+     * Input/Output
+     *
+     * [execution time limit] 3 seconds (java)
+     *
+     * [input] array.array.integer image
+     *
+     * An image, stored as a rectangular matrix of non-negative integers.
+     *
+     * Guaranteed constraints: 3 ≤ image.length ≤ 100, 3 ≤ image[0].length ≤ 100, 0
+     * ≤ image[i][j] ≤ 255.
+     *
+     * [output] array.array.integer
+     *
+     * A blurred image represented as integers, obtained through the process in the
+     * description.
+     */
 
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		int[][] image = {{1, 1, 1, 2, 4}, {1, 7, 1, 2, 4}, {1, 1, 1, 2, 4}, {1, 1, 1, 2, 4}, {1, 1, 1, 2, 4}};
-		System.out.println("Blur image: " + boxBlur(image)); 
+        int[][] image = {{7, 4, 0, 1}, {5, 6, 2, 2}, {6, 10, 7, 8}, {1, 4, 2, 0}};
+        System.out.println("Blur image: " + Arrays.deepToString(boxBlur(image)));
 
-	}
+    }
 
-	static int[][] boxBlur(int[][] image) {
+    private static int[][] boxBlur(int[][] image) {
 
-		int qtdXLinhas = image.length - 2;
-		int qtdXColunas = 0;
-		int qtdXTotal = 0;
-		
-		if (image[0].length > 3) {
-			qtdXColunas = image[0].length - 2;
-			qtdXTotal = qtdXLinhas * qtdXColunas;
-		} else {
-			qtdXTotal = qtdXLinhas;
-		}
-		
-		int count = 0;
-		int[][] blurImagem = new int[image.length][image[0].length];
-		while (count < qtdXTotal) {
-			int countXLinhas = 0;
-			while (countXLinhas < qtdXLinhas) {
-				
-				countXLinhas++;
-			}
-			count++;
-		}
+        int qtdHorizontalX = image.length - 2;
+        int qtdVerticalX;
+        int[][] blurImage;
 
-		System.out.println("Total: " + qtdXTotal);
-		return blurImagem;
-	}
+        if (image[0].length > 3) {
+            qtdVerticalX = image[0].length - 2;
+            blurImage = new int[qtdHorizontalX][qtdVerticalX];
+            int countX = 0;
+            int countY = 0;
+
+            while (countX < qtdHorizontalX) {
+                while (countY < qtdVerticalX) {
+                    int distorted = (image[countX][countY] + image[countX][countY + 1] + image[countX][countY + 2] +
+                            image[countX + 1][countY] + image[countX + 1][countY + 1] + image[countX + 1][countY + 2] +
+                            image[countX + 2][countY] + image[countX + 2][countY + 1] + image[countX + 2][countY + 2]) / 9;
+                    blurImage[countX][countY] = distorted;
+                    countY++;
+                }
+                countX++;
+                countY = 0;
+            }
+        } else {
+            blurImage = new int[qtdHorizontalX][1];
+            int count = 0;
+
+            while (count < qtdHorizontalX) {
+                int distorted = (image[count][0] + image[count][1] + image[count][2] +
+                        image[count + 1][0] + image[count + 1][1] + image[count + 1][2] +
+                        image[count + 2][0] + image[count + 2][1] + image[count + 2][2]) / 9;
+                blurImage[count][0] = distorted;
+                count++;
+            }
+        }
+
+        return blurImage;
+    }
 
 }
